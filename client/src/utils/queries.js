61de47dcfe22b5b91285fe343db9e64f9f5b41ef
elaine-luckey <emarieluckey@yahoo.com>
2023-11-08 09:17:23 -0500

    fixing bugs in existing code and adding mutations & queries files in the utils folder

---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+import { gql } from "@apollo/client";
+
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      email
+      username
+      bookCount
+      savedBooks {
+        authors
+        bookId
+        description
+        image
+        title
+        link
+      }
+    }
+  }
+`;
